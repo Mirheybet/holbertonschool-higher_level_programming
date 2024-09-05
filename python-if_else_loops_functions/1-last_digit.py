aec65a21abf1ca02d2cfe30524dcd106c7bd3c64
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-05 22:42:57 +0000

    lnx1-v5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 posetive = abs(number)
-if (posetive%10 == 0):
+if(posetive%10 == 0):
     print(f"Last digit of {number} is 0 and is 0")
 
 elif (number < 0):
-     posetive = (posetive % 10) * (-1)
-     print(f"Last digit of {number} is {posetive} and is less than 6 and not 0")
-
+    posetive = (posetive % 10) * ( -1 )
+    print(f"Last digit of {number} is {posetive} and is less than 6 and not 0")
 elif (number > 0):
     posetive = (posetive % 10)
     if (posetive > 5):
