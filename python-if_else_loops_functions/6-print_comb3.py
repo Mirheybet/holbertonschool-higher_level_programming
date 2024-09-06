35a4df19c3c6d69c80fade3033b5c05873406463
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-06 16:19:06 +0000

    lnx6-v3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(10):
         if (i > j or i == j):
             continue
         else:
-            if(i == 8 and j == 9):
+            if (i == 8 and j == 9):
                 print("{}{}".format(i, j), end="\n")
                 break
             print("{}{}".format(i, j), end=", ")
