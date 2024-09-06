e20d3b486705a86c2817e0ec3571f0df2b2c4956
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-06 18:05:37 +0000

    lnx8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_cln = ""
+    for ch in str:
+        if (ord(ch) in range(97, 123)):
+            str_cln += chr(ord(ch)-32)
+        else:
+            str_cln += ch
+    print(str_cln)
