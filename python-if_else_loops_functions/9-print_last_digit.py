bbe3a3fb000aeece929f63be25071d6010d9760a
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-06 18:33:47 +0000

    lnx9-v2

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,7 @@
 def print_last_digit(number):
     nmb_clone = abs(number)
     if (number < 0):
+        print("{}".format(nmb_clone % 10), end = "")
         return (nmb_clone % 10)
+    print("{}".format(nmb_clone % 10), end = "")
     return (number % 10)
