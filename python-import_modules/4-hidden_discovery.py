0fb28baa76f0cad11aaa34fa7858e1d5ca98b526
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-07 19:02:08 +0000

    lnx4-v5

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def sortlist(perline):
-    per_lines = dir(perline)
+if (__name__ == "__main__"):
+    import hidden_4
+    per_lines = dir(hidden_4)
     per_lines = sorted(per_lines)
     for i in per_lines:
         if (i[0:2] == "__"):
