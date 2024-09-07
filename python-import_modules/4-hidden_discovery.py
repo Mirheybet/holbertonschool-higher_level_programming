eb5d7f6bb59a5137f20436870d8a1291f3d0b4c0
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-07 19:04:24 +0000

    lnx4-v6

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if (__name__ == "__main__"):
-    import hidden_4
-    per_lines = dir(hidden_4)
-    per_lines = sorted(per_lines)
-    for i in per_lines:
-        if (i[0:2] == "__"):
-            continue
-        print("{}".format(i))
