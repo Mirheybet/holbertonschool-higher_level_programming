0ae856c8b2936151a0b1906a577bdc70715d8054
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-08 17:55:07 +0000

    lnx100-v2

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 if (__name__ == "__main__"):
-    from calculator_1 import add, sub, mul, div
+    import calculator_1
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    ops = {"+": calculator_1.add, "-": calculator_1.sub, "*":calculator_1.mul, "/": calculator_1.div}
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
