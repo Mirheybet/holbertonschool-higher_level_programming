a99491422ec73abdd85445cac434ad6864780860
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-11 13:31:15 +0000

    lnx0-v5

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if (mylist == []):
+    if (my_list == []):
         print("".format())
     for i in my_list:
             print("{:d}".format(i), end="\n")
