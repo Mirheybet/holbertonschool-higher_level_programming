1bf0194eba8c9e9d3e425736447a43f187542600
Mirheybet <mirheybet.hesenov@gmail.com>
2024-09-11 14:58:48 +0000

    lnx4-v2

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    cp_my_list = [i for i in my_list]
     if (idx < 0 or idx > len(my_list)):
-        return my_list
+        return cp_my_list
     cp_my_list = [i for i in my_list]
     cp_my_list[idx] = element
     return (cp_my_list)
