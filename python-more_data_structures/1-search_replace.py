7ead8afb29b12253584e8f700e574a51a12e7437
root <mirheybet.hesenov@gmail.com>
2024-09-14 19:34:52 +0000

    lnx1-v2

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cp_my_list = my_list
+    cp_my_list = my_list[:]
     for i in range(len(cp_my_list)):
         if cp_my_list[i] == search:
             cp_my_list[i] = replace
