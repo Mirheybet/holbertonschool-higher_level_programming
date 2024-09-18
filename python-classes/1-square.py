f312cd1fb64e05923d1442eb1609a609441482fa
root <mirheybet.hesenov@gmail.com>
2024-09-18 14:30:01 +0000

    lnx1-v1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    Write a class Square that defines a square by: (based on 0-square.py)
+    """
+    def __init__(self, __size):
+        self.__size = size
