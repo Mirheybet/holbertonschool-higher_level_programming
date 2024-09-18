b1b0725243e2c4d68ab08a28749a49a5bd33533f
root <mirheybet.hesenov@gmail.com>
2024-09-18 15:16:00 +0000

    lnx2-v1

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square():
+    """
+    Write a class Square that defines a square by: (based on 1-square.py)
+    """
+    def __init__(self, size=0):
+        """
+        errorhandling: size must be an integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        """
+        errorhandling: size must be >= 0
+        """
+        if not (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
