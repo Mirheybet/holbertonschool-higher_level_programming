46b8e53bf63f81c09b58d63cae9c5b84aa98371b
root <mirheybet.hesenov@gmail.com>
2024-09-18 14:51:49 +0000

    lnx1-v2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,5 +8,8 @@ class Square:
     """
     Write a class Square that defines a square by: (based on 0-square.py)
     """
-    def __init__(self, __size):
-        self.__size = size
+    def __init__(self, size):
+         """
+         Write a class Square that defines a square by: (based on 0-square.py
+         """
+         self.__size = size
