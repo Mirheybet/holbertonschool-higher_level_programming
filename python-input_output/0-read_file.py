b66d1be7c0255bd75e187392046b6d8479765613
root <mirheybet.hesenov@gmail.com>
2024-10-01 22:34:43 +0000

    lnx1-v3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+read_file
+"""
+
 
 def read_file(filename=""):
+    """
+    read file utf-8
+    """
+
     with open("my_file_0.txt", encoding='utf-8') as file:
         file = file.read()
         print(file, end="")
