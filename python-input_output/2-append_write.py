8460a536031e4818147f72f45de67979431b0c99
root <mirheybet.hesenov@gmail.com>
2024-10-01 23:20:56 +0000

    lnx3-v4

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,6 +8,6 @@ def append_write(filename="", text=""):
     """
     with
     """
-    with open("file_append.txt", "a", encoding='utf-8') as file:
+    with open(filename, "a", encoding='utf-8') as file:
         file = file.write(text)
         return file
