faeaf0b5338b1b213bf28c81a74d94bde581e109
root <mirheybet.hesenov@gmail.com>
2024-10-03 00:01:13 +0000

    lnx5-v5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,4 +10,5 @@ def save_to_json_file(my_obj, filename):
     duump with write
     """
     with open("my_dict.json", 'w', encoding= 'utf-8') as file:
-        file.write(js.dumps(my_obj))
+        file = file.write(js.dumps(my_obj))
+        return file
