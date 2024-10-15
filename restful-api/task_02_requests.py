28f29eb85c25f670da961f7f6002c7169ad01e51
root <mirheybet.hesenov@gmail.com>
2024-10-15 21:46:46 +0000

    task02-v1

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import json
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    posts_url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(posts_url)
+    print("Status Code:", response.status_code)
+
+    if (response.status_code != 200):
+        print("FAIL: def fetch_and_print_posts()")
+    else:
+        responses = response.json()
+        for response in responses:
+            print(response["title"])
+
+
+def fetch_and_save_posts():
+    posts_url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(posts_url)
+    print("Status Code:", response.status_code)
+
+    if (response.status_code != 200):
+        print("FAIL: def fetch_and_save_posts()")
+    else:
+        responses = response.json()
+        structured_posts = [{
+            'id': response['id'],
+            'title': response['title'],
+            'body': response['body']
+            } for response in responses]
+        with open("post.csv", "w", newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(structured_posts)
+            print("FILE:posts.csv created!")
