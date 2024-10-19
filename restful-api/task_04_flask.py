5ab12d5a07bd6566eda4bc79a84a7174633f8062
Mirheybet <102419525+Mirheybet@users.noreply.github.com>
2024-10-19 21:34:36 +0400

    task04-windows-v3

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,63 +1,50 @@
-from flask import *
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, 
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-    }
-
-@app.route("/")
-def main():
-    return ("Welcome to the Flask API!")
+all_users = {}
 
 
-@app.route("/status")
-def status():
-    return ("OK")
+@app.route('/')
+def home():
+    return 'Welcome to the Flask API!'
 
-@app.route("/data")
+
+@app.route('/data')
 def data():
-    return (jsonify(list(users.keys())))
+    usernames = list(all_users)
+    return jsonify(usernames)
 
-@app.route("/users/<username>")
-def usernames(username):
-    user = users.get(username)
-    if user:
-        return (jsonify(user))
-    else:
-        return (jsonify({"error": "User not found"})), 404
 
-@app.route("/add_user", methods=["POST"])
+@app.route('/add_user', methods=['POST'])
 def add_user():
-    data = request.get_json()
-    if("username" not in data):
-        return jsonify({"error": "Username is required"}), 400
-    else:
-        usernames = data['username']
-        if usernames in users:
-            return jsonify({"error": "User already exists"}), 400
-        
-        users[usernames] = {
-                        "username": data["username"],
-                        "name": data.get("name", ""),
-                        "age": data.get("age", 0),
-                        "city": data.get("city", "")
-                        }
-        return jsonify({
-        "message": "User added",
-        "user": users[usernames]
-    }), 201
-        
-@app.route("/delete_user/<username>", methods=["DELETE"])
-def delete_user(username):
-    # Kullanıcı var mı kontrol et
-    if username in users:
-        # Kullanıcıyı sil
-        del users[username]
-        return jsonify({"message": f"User '{username}' has been deleted"}), 200
-    else:
-        return jsonify({"error": "User not found"}), 404
+    data = request.json
+    if data is None or data.get('username') is None:
+        return jsonify({'error': 'Username is required'}), 400
+    user = {
+        'username': data.get('username'),
+        'name': data.get('name'),
+        'age': data.get('age'),
+        'city': data.get('city')
+    }
+    all_users[user.get('username')] = user
+    return jsonify({'message': 'User added', 'user': user}), 201
 
-        
+
+@app.route('/users/<username>')
+def user(username):
+    if username is None:
+        return jsonify({'error': 'Username is required'}), 400
+    if username not in all_users:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(all_users[username])
+
+
+@app.route('/status')
+def status():
+    return "OK"
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
