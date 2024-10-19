b2e99382c5d41b6ec92b87aa4833fc41c4cdb52b
Mirheybet <102419525+Mirheybet@users.noreply.github.com>
2024-10-19 21:20:25 +0400

    task4-windows-v1

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,52 @@
+from flask import *
+app = Flask(__name__)
+
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, 
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+    }
+
+@app.route("/")
+def main():
+    return ("Welcome to the Flask API!")
+
+
+@app.route("/status")
+def status():
+    return ("OK")
+
+@app.route("/data")
+def data():
+    return (jsonify(list(users.keys())))
+
+@app.route("/users/<username>")
+def usernames(username):
+    user = users.get(username)
+    if user:
+        return (jsonify(user))
+    else:
+        return (jsonify({"error": "User not found"})), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    if("username" not in data):
+        return jsonify({"error": "Username is required"}), 400
+    else:
+        usernames = data['username']
+        if usernames in users:
+            return jsonify({"error": "User already exists"}), 400
+        
+        users[usernames] = {
+                        "username": data["username"],
+                        "name": data.get("name", ""),
+                        "age": data.get("age", 0),
+                        "city": data.get("city", "")
+                        }
+        return jsonify({
+        "message": "User added",
+        "user": users[usernames]
+    }), 201
+        
+if __name__ == "__main__":
+    app.run(debug=True)
