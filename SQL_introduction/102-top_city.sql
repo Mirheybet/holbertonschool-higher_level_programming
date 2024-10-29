bbc5fe61eb37f2c60d87658130a800fa2d53bf6c
root <mirheybet.hsenov@gmail.com>
2024-10-29 21:15:33 +0000

    lnx102-v3

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Script.
-SELECT city,AVG(temprature) AS avg_temp FROM tempratures
+SELECT city,AVG(value) AS avg_temp FROM tempratures
 GROUP BY city WHERE IN month(7,8)
 ORDER BY temprature DESC
 LIMIT 3;
