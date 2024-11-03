c4a874f1594419a9e219245ecb4dc44ae90e5f65
root <mirheybet.hesenov@gmail.com>
2024-11-03 18:34:32 +0000

    lnx3-v2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,16 +2,11 @@
 """
 Connect DB  , check cli 
 """
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM `states`""")
-
-    [print(state) for state in cur.fetchall() if state[1] == argv[4]]
-
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
