5d0ecfe83799a08cb9858c96af9d64a3332ace61
root <mirheybet.hesenov@gmail.com>
2024-11-03 18:30:15 +0000

    lnx10-v2

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-Connect DB 
+Connect DB
 """
-
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if state.name == argv[4]:
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
             print("{}".format(state.id))
+            found = True
             break
-    else:
+    if found is False:
         print("Not found")
